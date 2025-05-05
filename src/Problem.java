729a39764460cf022f49afeaf11a383d646ccbb3
mauriciopolvora <mauricio.polvora@gmail.com>
2025-05-05 12:29:43 +0100

    implementation with just UnionFind kruskal

--- src/Problem.java
+++ src/Problem.java
@@ -1,22 +1,32 @@
+import UnionFind.UnionFind;
 import UnionFind.UnionFindInArray;
 import java.util.*;
 
 public class Problem {
     private final List<Road> roads;
+    private boolean isSorted = false;
     private final int numLocations;
 
     public Problem(int numLocations) {
-        this.numLocations = numLocations;
         this.roads = new ArrayList<>();
+        this.numLocations = numLocations;
+
     }
 
     public void addRoad(int l1, int l2, int h) {
         roads.add(new Road(l1, l2, h));
+        isSorted = false;
+    }
+
+    private void sortRoads() {
+        if (!isSorted) {
+            roads.sort(Comparator.comparingInt(Road::getH));
+            isSorted = true;
+        }
     }
 
     public int findShortestPath(int start, int destination) {
-
-        roads.sort(Comparator.comparingInt(Road::getH));
+        sortRoads();
 
         UnionFindInArray uf = new UnionFindInArray(numLocations);
 
